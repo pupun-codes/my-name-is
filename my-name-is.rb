036e39fa71c976d5e7a6a33fc2ab20b181eb05af
pupun-codes <mail.to.pupun@gmail.com>
2025-06-26 12:23:58 -0400

    Update Homebrew formula for v0.1.1

--- my-name-is.rb
+++ my-name-is.rb
@@ -1,7 +1,7 @@
 class MyNameIs < Formula
   desc "A command-line tool for generating whimsical names"
   homepage "https://github.com/user/my-name-is"
-  url "https://github.com/user/my-name-is/archive/v0.1.0.tar.gz"
+  url "https://github.com/pupun-codes/my-name-is/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "..."
 
   def install
